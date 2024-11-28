4547e3f0ae2353580ae6a1b7aa2d7ba7d7a3d95b
GamGomYang <ay6656@naver.com>
2024-11-29 02:58:24 +0900

    prototype version add

--- typing_game/prototype/version/client2.c
+++ typing_game/prototype/version/client2.c
@@ -7,7 +7,7 @@
 
 #define SERVER_IP "127.0.0.1"
 #define PORT 12345
-#define BUF_SIZE 1024
+#define BUF_SIZE 2048 // 서버에서 보내는 큰 메시지를 수신하기 위해 크기를 늘림
 
 void *receive_messages(void *arg) {
     int client_socket = *(int *)arg;
@@ -17,11 +17,16 @@ void *receive_messages(void *arg) {
         memset(buffer, 0, BUF_SIZE);
         int len = recv(client_socket, buffer, BUF_SIZE - 1, 0);
         if (len <= 0) {
-            printf("서버 연결 종료.\n");
+            if (len == 0) {
+                printf("서버가 연결을 종료했습니다.\n");
+            } else {
+                perror("recv 오류");
+            }
             close(client_socket);
             exit(1);
         }
-        printf("%s", buffer); // 서버 메시지 출력
+        buffer[len] = '\0';           // 수신된 데이터의 끝에 널 문자 추가
+        printf("서버: %s\n", buffer); // 서버 메시지 출력
     }
     return NULL;
 }
@@ -53,18 +58,28 @@ int main() {
     printf("서버에 연결되었습니다!\n");
 
     // 서버 메시지 수신 스레드 생성
-    pthread_create(&receive_thread, NULL, receive_messages, &client_socket);
+    if (pthread_create(&receive_thread, NULL, receive_messages, &client_socket) != 0) {
+        perror("스레드 생성 실패");
+        close(client_socket);
+        exit(1);
+    }
     pthread_detach(receive_thread);
 
     // 사용자 입력 처리
     while (1) {
         memset(input, 0, BUF_SIZE);
         printf("단어 입력: ");
-        fgets(input, BUF_SIZE, stdin);
+        if (fgets(input, BUF_SIZE, stdin) == NULL) {
+            printf("입력 오류 또는 EOF 발생.\n");
+            break;
+        }
         input[strcspn(input, "\n")] = '\0'; // 개행 문자 제거
 
         // 서버에 입력 전송
-        send(client_socket, input, strlen(input), 0);
+        if (send(client_socket, input, strlen(input), 0) == -1) {
+            perror("send 오류");
+            break;
+        }
     }
 
     close(client_socket);
--- typing_game/prototype/version/server1.c
+++ typing_game/prototype/version/server1.c
@@ -8,22 +8,42 @@
 #define PORT 12345
 #define MAX_CLIENTS 2
 #define BUF_SIZE 1024
+#define MAX_WORDS_PER_CATEGORY 50
+#define NUM_CATEGORIES 3
 
 // 단어 카테고리 및 데이터
 const char *categories[] = {"동물", "나라", "사자성어"};
-const char *words[][10] = {
-    {"기린", "코끼리", "사자", "얼룩말", "강아지"},              // 동물
-    {"한국", "미국", "일본", "중국", "프랑스"},                  // 나라
-    {"유비무환", "천고마비", "동병상련", "일석이조", "오리무중"} // 사자성어
-};
-int word_counts[] = {5, 5, 5}; // 각 카테고리의 단어 개수
+const char *words[NUM_CATEGORIES][MAX_WORDS_PER_CATEGORY] = {
+    // 동물 카테고리 단어 50개 (예시로 임의의 동물 이름을 사용했습니다)
+    {
+        "기린", "코끼리", "사자", "얼룩말", "강아지", "고양이", "호랑이", "여우", "늑대", "곰",
+        "다람쥐", "토끼", "햄스터", "사슴", "코알라", "캥거루", "하마", "코뿔소", "침팬지", "고릴라",
+        "팬더", "돌고래", "상어", "고래", "오징어", "문어", "게", "가재", "새우", "복어",
+        "메기", "붕어", "송어", "연어", "잉어", "독수리", "매", "참새", "비둘기", "까치",
+        "까마귀", "오리", "거위", "닭", "칠면조", "부엉이", "올빼미", "앵무새", "타조", "플라밍고"},
+    // 나라 카테고리 단어 50개 (예시로 임의의 나라 이름을 사용했습니다)
+    {
+        "한국", "미국", "일본", "중국", "프랑스", "독일", "영국", "이탈리아", "스페인", "포르투갈",
+        "러시아", "캐나다", "멕시코", "브라질", "아르헨티나", "호주", "뉴질랜드", "인도", "파키스탄", "이란",
+        "이라크", "이집트", "남아프리카공화국", "나이지리아", "케냐", "탄자니아", "모로코", "알제리", "터키", "그리스",
+        "네덜란드", "벨기에", "스웨덴", "노르웨이", "덴마크", "핀란드", "폴란드", "체코", "오스트리아", "스위스",
+        "헝가리", "루마니아", "불가리아", "우크라이나", "벨라루스", "카자흐스탄", "우즈베키스탄", "몽골", "베트남", "태국"},
+    // 사자성어 카테고리 단어 50개 (예시로 임의의 사자성어를 사용했습니다)
+    {
+        "유비무환", "천고마비", "동병상련", "일석이조", "오리무중", "구사일생", "권선징악", "고진감래", "각골난망", "감언이설",
+        "격세지감", "견물생심", "과유불급", "금상첨화", "기고만장", "대기만성", "동문서답", "동상이몽", "마이동풍", "백문불여일견",
+        "배은망덕", "백발백중", "사면초가", "산전수전", "새옹지마", "설상가상", "속수무책", "수수방관", "안하무인", "양자택일",
+        "어부지리", "오합지졸", "우왕좌왕", "유유자적", "일사천리", "일취월장", "임기응변", "자업자득", "적반하장", "전전긍긍",
+        "천진난만", "청출어람", "침소봉대", "타산지석", "탁상공론", "파죽지세", "학수고대", "호랑이도 제 말하면 온다", "화룡점정", "환골탈태"}};
+
+int word_counts[NUM_CATEGORIES] = {50, 50, 50}; // 각 카테고리의 단어 개수
 
 int client_sockets[MAX_CLIENTS] = {0};
 pthread_mutex_t lock;
 int connected_clients = 0;
 pthread_cond_t start_cond = PTHREAD_COND_INITIALIZER;
 
-char game_words[10][BUF_SIZE]; // 선택된 단어 저장
+char game_words[MAX_WORDS_PER_CATEGORY][BUF_SIZE]; // 선택된 단어 저장
 int word_count = 0;
 int scores[MAX_CLIENTS] = {0};
 
@@ -38,7 +58,7 @@ void broadcast(const char *message) {
 
 // 단어 리스트와 점수 브로드캐스트
 void broadcast_words_and_scores() {
-    char buffer[BUF_SIZE];
+    char buffer[BUF_SIZE * 2] = {0};
     snprintf(buffer, sizeof(buffer), "남은 단어: ");
     for (int i = 0; i < word_count; i++) {
         strcat(buffer, game_words[i]);
@@ -56,9 +76,9 @@ void broadcast_words_and_scores() {
 }
 
 void select_category(int client_id) {
-    char buffer[BUF_SIZE];
+    char buffer[BUF_SIZE] = {0};
     snprintf(buffer, sizeof(buffer), "카테고리를 선택하세요:\n");
-    for (int i = 0; i < sizeof(categories) / sizeof(categories[0]); i++) {
+    for (int i = 0; i < NUM_CATEGORIES; i++) {
         char line[BUF_SIZE];
         snprintf(line, sizeof(line), "%d. %s\n", i + 1, categories[i]);
         strcat(buffer, line);
@@ -70,7 +90,7 @@ void select_category(int client_id) {
     recv(client_sockets[client_id], buffer, BUF_SIZE, 0);
     int category_index = atoi(buffer) - 1;
 
-    if (category_index >= 0 && category_index < (sizeof(categories) / sizeof(categories[0]))) {
+    if (category_index >= 0 && category_index < NUM_CATEGORIES) {
         snprintf(buffer, sizeof(buffer), "클라이언트%d가 '%s'를 선택했습니다. 게임을 시작합니다.\n",
                  client_id + 1, categories[category_index]);
         broadcast(buffer);
@@ -203,6 +223,7 @@ void *handle_client(void *arg) {
     close(client_socket);
     pthread_mutex_lock(&lock);
     client_sockets[client_id] = 0;
+    connected_clients--;
     pthread_mutex_unlock(&lock);
 
     printf("클라이언트 %d 연결 종료\n", client_id + 1);
