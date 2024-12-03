d64cafcbff206c6246140eb29dd4f4e3464dd269
wylee <ebmmqmf@naver.com>
2024-12-04 00:44:43 +0900

    feat: 다빈치코드 5차 빌드

--- coda_module/davinci.c
+++ coda_module/davinci.c
@@ -17,8 +17,8 @@ typedef struct {
     int num_tiles;
 } Player;
 
-Tile deck[TOTAL_TILES]; 
-int deck_index = 0; 
+Tile deck[TOTAL_TILES];
+int deck_index = 0;
 
 int compare_tiles(const void *a, const void *b) {
     Tile *tileA = (Tile *)a;
@@ -43,7 +43,7 @@ void shuffle_deck() {
 
 void initialize_game(Player players[]) {
     int index = 0;
-    
+
     for (int i = 1; i <= MAX_TILES; i++) {
         deck[index].number = i;
         deck[index].color = 'B';
@@ -54,19 +54,16 @@ void initialize_game(Player players[]) {
         deck[index++].revealed = 0;
     }
 
-
     shuffle_deck();
 
-    
+    int tile_index = 0;
     for (int i = 0; i < MAX_PLAYERS; i++) {
-        players[i].num_tiles = 4; 
+        players[i].num_tiles = 4;
         for (int j = 0; j < players[i].num_tiles; j++) {
             players[i].tiles[j] = deck[tile_index++];
         }
 
-
         qsort(players[i].tiles, players[i].num_tiles, sizeof(Tile), compare_tiles);
-
 
         printf("Player %d's tiles:\n", i + 1);
         for (int j = 0; j < players[i].num_tiles; j++) {
@@ -75,49 +72,45 @@ void initialize_game(Player players[]) {
         printf("\n");
     }
 
-    deck_index = tile_index; 
+    deck_index = tile_index;
 }
-
 
 void display_tiles(Player player) {
     printf("Your tiles: ");
     for (int i = 0; i < player.num_tiles; i++) {
         if (player.tiles[i].revealed) {
-            printf("[%c%d] ", player.tiles[i].color, player.tiles[i].number); 
+            printf("[%c%d] ", player.tiles[i].color, player.tiles[i].number);
         } else {
-            printf("[?%c?] ", player.tiles[i].color); 
+            printf("[?%c?] ", player.tiles[i].color);
         }
     }
     printf("\n");
 }
 
-
 void draw_tile(Player *player) {
     if (deck_index < TOTAL_TILES) {
         player->tiles[player->num_tiles++] = deck[deck_index++];
-        qsort(player->tiles, player->num_tiles, sizeof(Tile), compare_tiles); 
+        qsort(player->tiles, player->num_tiles, sizeof(Tile), compare_tiles);
         printf("Drew a new tile!\n");
     } else {
         printf("No more tiles to draw.\n");
     }
 }
 
-
 void display_opponent_tiles(Player opponent) {
     printf("Opponent's tiles: ");
     for (int i = 0; i < opponent.num_tiles; i++) {
         if (opponent.tiles[i].revealed) {
-            printf("[%c%d] ", opponent.tiles[i].color, opponent.tiles[i].number); 
+            printf("[%c%d] ", opponent.tiles[i].color, opponent.tiles[i].number);
         } else {
-            printf("[%d ?%c?] ", i + 1, opponent.tiles[i].color); 
+            printf("[%d ?%c?] ", i + 1, opponent.tiles[i].color);
         }
     }
     printf("\n");
 }
 
-
 int guess_tile(Player *opponent, int index, char color, int number) {
-    index--; 
+    index--;
     if (index < 0 || index >= opponent->num_tiles) {
         printf("Invalid index. Please try again.\n");
         return 0;
@@ -132,7 +125,6 @@ int guess_tile(Player *opponent, int index, char color, int number) {
     }
 }
 
-
 int check_win(Player *opponent) {
     for (int i = 0; i < opponent->num_tiles; i++) {
         if (!opponent->tiles[i].revealed) {
@@ -142,15 +134,14 @@ int check_win(Player *opponent) {
     return 1;
 }
 
-
 void play_game(Player players[]) {
     int turn = 0;
     while (1) {
         int guess_index, guess_number;
         char guess_color;
-        
+
         printf("\nPlayer %d's turn\n", turn + 1);
-        draw_tile(&players[turn]); 
+        draw_tile(&players[turn]);
         display_tiles(players[turn]);
         display_opponent_tiles(players[1 - turn]);
 
@@ -171,10 +162,7 @@ void play_game(Player players[]) {
             }
         } else {
             draw_tile(&players[turn]);
-            turn = 1 - turn; 
+            turn = 1 - turn;
         }
     }
 }
-
-
-
